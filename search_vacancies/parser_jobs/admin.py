aced6a15a8646eb225ba00fb1ebb54b2cfbc46e3
Alexei Bolibok <insamo217@yandex.ru>
2020-11-30 20:07:41 +0300

    edited admin.py

--- search_vacancies/parser_jobs/admin.py
+++ search_vacancies/parser_jobs/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 from .models import *
-from .forms import VacanciesForm
 
-#@admin.register(Vacancies)
-#class VacanciesAdmin(admin.ModelAdmin):
-    #list_display = ('pk', 'title', 'url', 'text', 'category', 'source')
-    #form = VacanciesForm
 
 admin.site.register(Vacancies)
 admin.site.register(Category)
